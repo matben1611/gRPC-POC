e69f51c910522ee61e9921db96d1e1b78e39393a
Mathis <mathisbenecke@icloud.com>
2024-03-19 11:22:23 +0100

    add network test and local container

--- gRPC/app/server.py
+++ gRPC/app/server.py
@@ -7,6 +7,9 @@ import calculator_pb2
 import calculator_pb2_grpc 
 import location_pb2
 import location_pb2_grpc
+import network_pb2
+import network_pb2_grpc
+import psutil
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
@@ -21,6 +24,19 @@ class LocationStreamerServicer(location_pb2_grpc.LocationStreamerServicer):
             )
             yield location_info
 
+class NetworkTrafficServicer(network_pb2_grpc.NetworkTrafficServicer):
+    def StreamNetworkInfo(self, request_iterator, context):
+        while True:
+            network_traffic = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
+            bandwidth = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
+            network_info = network_pb2.NetworkResponse(
+                current_traffic=network_traffic,
+                bandwidth=bandwidth
+            )
+            yield network_info
+            time.sleep(3)  # Stream alle 3 Sekunden aktualisieren
+
+
 class CalculatorServicer(calculator_pb2_grpc.CalculatorServicer):
     def AddNumbers(self, request, context):
         result = request.x + request.y
@@ -30,6 +46,7 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     location_pb2_grpc.add_LocationStreamerServicer_to_server(LocationStreamerServicer(), server)
     calculator_pb2_grpc.add_CalculatorServicer_to_server(CalculatorServicer(), server)
+    network_pb2_grpc.add_NetworkTrafficServicer_to_server(NetworkTrafficServicer(), server)  # Fügen Sie den Netzwerk-Traffic-Dienst hinzu
     server.add_insecure_port('[::]:50051')
     server.start()
     print("Server started. Listening on port 50051.")
--- gRPC/app/streamer.py
+++ gRPC/app/streamer.py
@@ -2,6 +2,20 @@ import grpc
 import location_pb2
 import location_pb2_grpc
 import time
+import network_pb2
+import network_pb2_grpc
+
+def run_network_client():
+    channel = grpc.insecure_channel('localhost:50052')
+    stub = network_pb2_grpc.NetworkTrafficStub(channel)
+    try:
+        responses = stub.StreamNetworkInfo(iter([network_pb2.NetworkRequest()]))
+        for response in responses:
+            print(f"Aktueller Netzwerkverkehr: {response.current_traffic} bytes")
+            print(f"Bandbreite: {response.bandwidth} bytes")
+            print("-" * 20)
+    except grpc.RpcError as e:
+        print(f"Fehler beim Streamen von Netzwerkinformationen: {e}")
 
 def run_location_client():
     channel = grpc.insecure_channel('localhost:50051')
@@ -19,4 +33,5 @@ def run_location_client():
         print(f"Fehler beim Streamen von Ort- und Zeitinformationen: {e}")
 
 if __name__ == '__main__':
-    run_location_client()
+    #run_location_client()
+    run_network_client()
